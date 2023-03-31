6de66d8ef3d1404ecef628073d515615590f119e
brittoeduardo <111761860+brittoeduardo@users.noreply.github.com>
2023-03-31 16:57:08 -0300

    arrumado

---
+++ Modulo_4/TesteChartJS/config.js
@@ -0,0 +1,4 @@
+var config = {
+    accessToken: 'pk.eyJ1IjoiZWR1YXJkb2JyaXR0byIsImEiOiJja3o0NzFiNTQwYnJuMm5wdmE5NXpzcXZiIn0.J5F7CF5FVoqV4_193P7hig',
+    subscriptionKey: 'azure maps subscription key goes here'
+};
